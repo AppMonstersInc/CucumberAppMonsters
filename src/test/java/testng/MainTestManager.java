59235df6874ef7b680a36e5a7c819118c3a8cf1f
Jack Steven <jurabek.sayfiev2@gmail.com>
2020-02-14 14:39:25 -0600

    jurabek updated develop

--- src/test/java/testng/MainTestManager.java
+++ src/test/java/testng/MainTestManager.java
@@ -1,12 +1,14 @@
 package testng;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.InventoryAdjustmentsPage;
 import pages.ReorderingRulesPage;
 import utilities.BriteUtils;
 import utilities.Driver;
@@ -18,7 +20,7 @@ import java.util.List;
 public class MainTestManager {
     HomePage homePage = new HomePage();
     ReorderingRulesPage reorderingRulesPage = new ReorderingRulesPage();
-
+    InventoryAdjustmentsPage inventoryAdjustmentsPage = new InventoryAdjustmentsPage();
 
     @BeforeClass
     public void login() {
@@ -99,6 +101,72 @@ public class MainTestManager {
         }
 
         Assert.assertEquals(count, 0, "Failed, all checkboxes should be enabled by default");
+
+    }
+    /*
+    manager should see inventory adjustments from Dash Op in the left side
+    manager should be able to click Inventory Adjustments button
+    manager has to see import, create and search button
+    as a manager when i click above three buttons, i have to be able to use them
+     */
+    @Test(priority = 5)  // Jurabek
+    public void inventoryAdjustButton() {
+        homePage.inventoryModuleTab.click();
+        // verify inventory adjustments button
+        SeleniumUtils.pause(2);
+        Assert.assertTrue(inventoryAdjustmentsPage.inventoryModuleDisplayVerify.isDisplayed(),
+                "Inventory Adjustments module is NOT displayed!!!");
+        // inventory adjustments click
+        inventoryAdjustmentsPage.inventoryAdjustmentsButton.click();
+    }
+    @Test(priority = 6)
+            public void createButton() {
+        // inside inventory adjsutments module create button is displayed
+        Assert.assertTrue(inventoryAdjustmentsPage.createButtonVerify.isDisplayed(),
+                "Create button is NOT displayed!!!");
+        // click to create button
+        inventoryAdjustmentsPage.createButtonVerify.click();
+        // accept alert button
+        inventoryAdjustmentsPage.alertAcceptButton.click();
+    }
+    @Test(priority = 7)
+            public void importButton() {
+        // click to import button
+        inventoryAdjustmentsPage.importButtonVerify.click();
+        // to go back to inventory adjustments page
+        inventoryAdjustmentsPage.cancelButtonPress.click();
+    }
+    @Test(priority = 8)
+            public void searchBox(){
+        // search button verify
+        Assert.assertTrue(inventoryAdjustmentsPage.searchBoxVerify.isDisplayed(),
+                "search button is NOT displayed!!!");
+        inventoryAdjustmentsPage.searchBoxVerify.click();
+
+    }
+    /*
+    as a manager i can navigate to search button in the inventory adjustments page
+    as a manager when i click search button it should allow us to search products
+    as a manager when i should type search button
+    as a manager i can click the any offered products to choose
+     */
+    @Test(priority = 9) // Jurabek
+    public void searchButtonVerifyManager(){
+        // go to Inventory page to start from the beginning
+        //  homePage.inventoryModuleTab.click();
+//        inventoryAdjustmentsPage.inventoryAdjustmentsButton.click();
+//        // go to inventory adjustments module again
+//        Assert.assertTrue(inventoryAdjustmentsPage.inventoryModuleDisplayVerify.isDisplayed(),
+//                "Inventory Adjustments button is NOT displayed!");
+        inventoryAdjustmentsPage.inventoryAdjustmentsButton.click();
+        // go to search button
+        Assert.assertTrue(inventoryAdjustmentsPage.searchBoxVerify.isDisplayed(),
+                "Search button is NOT displayed!!!");
+        // search any kind of item
+        SeleniumUtils.pause(2);
+        inventoryAdjustmentsPage.searchBoxVerify.sendKeys("apple" + Keys.ENTER + Keys.ENTER);
+        Assert.assertTrue(inventoryAdjustmentsPage.firstTextVerify.isDisplayed(),
+                "First item is NOT displayed!!!");
 
     }
 
